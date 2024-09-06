46aa857a6bdbcd32cdd60c259d5155ef3e9260d4
William Wun <williamwun@hotmail.com>
2024-09-06 16:21:03 -0700

    cypress pending

--- 52-Testing-Cypress/js/app.js
+++ 52-Testing-Cypress/js/app.js
@@ -1,3 +1,9 @@
+//cypress también usa npm, se inicia con npm init
+//se instala cypress con npm i --save-dev cypress
+//aquí se usa npx cypress open, esto abre la aplicación de escritorio y crea una carpeta cypress
+
+//cypress ha cambiado mucho desde el video, es mejor aprenderlo desde otro lado, queda pendiente
+
 import App from './classes/App.js';
 
 const app = new App();
